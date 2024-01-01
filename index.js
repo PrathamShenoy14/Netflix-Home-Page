4967739482901c40aec1670402d85e696a6ebb5e
PrathamShenoy14 <140513705+PrathamShenoy14@users.noreply.github.com>
2024-01-01 01:21:02 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+
+document.querySelectorAll(".dropdown")[0].addEventListener("click",function (){
+    if(document.querySelectorAll(".dropdown-content")[0].classList.contains("show")){
+        document.querySelectorAll(".dropdown-content")[0].classList.remove("show")
+    }
+    else{
+        for(var i=0;i<6;i++){
+            if(document.querySelectorAll(".dropdown-content")[i].classList.contains("show")){
+                document.querySelectorAll(".dropdown-content")[i].classList.remove("show")
+                break;
+            }
+            else{
+                continue;
+            }    
+        }
+        document.querySelectorAll(".dropdown-content")[0].classList.add("show")
+    }
+        
+});
+
+document.querySelectorAll(".dropdown")[1].addEventListener("click",function (){
+    if(document.querySelectorAll(".dropdown-content")[1].classList.contains("show")){
+        document.querySelectorAll(".dropdown-content")[1].classList.remove("show")
+    }
+    else{
+        for(var i=0;i<6;i++){
+            if(document.querySelectorAll(".dropdown-content")[i].classList.contains("show")){
+                document.querySelectorAll(".dropdown-content")[i].classList.remove("show")
+                break;
+            }
+            else{
+                continue;
+            }    
+        }
+        document.querySelectorAll(".dropdown-content")[1].classList.add("show")
+    }
+        
+});
+
+document.querySelectorAll(".dropdown")[2].addEventListener("click",function (){
+    if(document.querySelectorAll(".dropdown-content")[2].classList.contains("show")){
+        document.querySelectorAll(".dropdown-content")[2].classList.remove("show")
+    }
+    else{
+        for(var i=0;i<6;i++){
+            if(document.querySelectorAll(".dropdown-content")[i].classList.contains("show")){
+                document.querySelectorAll(".dropdown-content")[i].classList.remove("show")
+                break;
+            }
+            else{
+                continue;
+            }    
+        }
+        document.querySelectorAll(".dropdown-content")[2].classList.add("show")
+    }
+        
+});
+
+document.querySelectorAll(".dropdown")[3].addEventListener("click",function (){
+    if(document.querySelectorAll(".dropdown-content")[3].classList.contains("show")){
+        document.querySelectorAll(".dropdown-content")[3].classList.remove("show")
+    }
+    else{
+        for(var i=0;i<6;i++){
+            if(document.querySelectorAll(".dropdown-content")[i].classList.contains("show")){
+                document.querySelectorAll(".dropdown-content")[i].classList.remove("show")
+                break;
+            }
+            else{
+                continue;
+            }    
+        }
+        document.querySelectorAll(".dropdown-content")[3].classList.add("show")
+    }
+        
+});
+
+document.querySelectorAll(".dropdown")[4].addEventListener("click",function (){
+    if(document.querySelectorAll(".dropdown-content")[4].classList.contains("show")){
+        document.querySelectorAll(".dropdown-content")[4].classList.remove("show")
+    }
+    else{
+        for(var i=0;i<6;i++){
+            if(document.querySelectorAll(".dropdown-content")[i].classList.contains("show")){
+                document.querySelectorAll(".dropdown-content")[i].classList.remove("show")
+                break;
+            }
+            else{
+                continue;
+            }    
+        }
+        document.querySelectorAll(".dropdown-content")[4].classList.add("show")
+    }
+        
+});
+
+document.querySelectorAll(".dropdown")[5].addEventListener("click",function (){
+    if(document.querySelectorAll(".dropdown-content")[5].classList.contains("show")){
+        document.querySelectorAll(".dropdown-content")[5].classList.remove("show")
+    }
+    else{
+        for(var i=0;i<6;i++){
+            if(document.querySelectorAll(".dropdown-content")[i].classList.contains("show")){
+                document.querySelectorAll(".dropdown-content")[i].classList.remove("show")
+                break;
+            }
+            else{
+                continue;
+            }    
+        }
+        document.querySelectorAll(".dropdown-content")[5].classList.add("show")
+    }
+        
+});
+
